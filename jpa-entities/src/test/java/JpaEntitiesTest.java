02409e9d1fbd6c146a2ad83c5a8c6188edcfcee2
caruso local <kazoo6723@gmail.com>
2021-11-20 09:41:09 -0500

    added junit jupiter dependencies to parent and jpa entities module

--- jpa-entities/src/test/java/JpaEntitiesTest.java
+++ jpa-entities/src/test/java/JpaEntitiesTest.java
@@ -1,14 +1,15 @@
-import static org.junit.Assert.assertEquals;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+
+
 
 class JpaEntitiesTest {
 
 	@Test
 	void test() {
 		String hello = "Hello";
-		assertEquals("Hello", hello);
 	}
 
 }
