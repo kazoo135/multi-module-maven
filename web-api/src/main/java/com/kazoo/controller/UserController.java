2d9a9c4b671091f8c4291128f4ce7d512bd4d160
caruso local <kazoo6723@gmail.com>
2021-12-12 17:49:01 -0500

    added web-app module and updated web-api xsd

--- web-api/src/main/java/com/kazoo/controller/UserController.java
+++
@@ -1,7 +0,0 @@
-package com.kazoo.controller;
-
-public class UserController {
-	
-	
-
-}
---
+++ web-app/src/main/java/com/kazoo/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.kazoo.controllers;
+
+import com.kazoo.domain.UserCommand;
+import com.kazoo.entities.User;
+
+
+public class UserController {
+	
+	User saveUser(UserCommand userCommand) {
+		return new User();
+	}
+
+}
