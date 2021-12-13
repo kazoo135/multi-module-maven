0bfe2f1171fcb376b9d30b74ee4ef81d1d7f8eb8
caruso local <kazoo6723@gmail.com>
2021-12-13 18:42:27 -0500

    added web-app module

---
+++ web-app/src/main/java/com/kazoo/controllers/UserController.java
@@ -0,0 +1,13 @@
+package com.kazoo.controllers;
+
+import com.kazoo.domain.UserCommand;
+import com.kazoo.entities.User;
+
+public class UserController {
+
+	User saveUser(UserCommand userCommand) {
+	
+		// fake impl
+		return new User();
+	}
+}
