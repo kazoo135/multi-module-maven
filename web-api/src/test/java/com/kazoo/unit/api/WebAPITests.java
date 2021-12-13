3f22ef658273b1a0eb656fb226921e13fdbced79
caruso local <kazoo6723@gmail.com>
2021-12-13 17:47:47 -0500

    added web-api module

---
+++ web-api/src/test/java/com/kazoo/unit/api/WebAPITests.java
@@ -0,0 +1,14 @@
+package com.kazoo.unit.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WebAPITests {
+
+	@Test
+	void test() {
+		assertEquals('a', 'a');
+	}
+
+}
